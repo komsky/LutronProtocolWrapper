062499bba5b535a9dec6a7216bd53e4e8bc04c8e
Zuko Mgwili <zukomgwili@gmail.com>
2019-01-13 23:38:19 +0200

    feat: add mock my room plus client and mock my room device

---
+++ MockClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Autofac;
+using Lutron.Common.Interfaces;
+using Lutron.Connector;
+using Lutron.Service;
+
+namespace MockClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<OutputService>().As<IOutputService>();
+            builder.RegisterType<MyRoomPlusConnector>().As<IMyRoomPlusConnector>();
+            builder.RegisterType<MyRoomPlusClient>().As<IMyRoomPlusClient>();
+            builder.RegisterType<MyRoomPlusClientConfiguration>().As<IMyRoomPlusClientConfiguration>();
+            var container = builder.Build();
+
+            using (var lifetimeScope = container.BeginLifetimeScope())
+            {
+                var outputService = lifetimeScope.Resolve<IOutputService>();
+                var outputLevel = outputService.GetOutputLevel(2);
+                Console.WriteLine(outputLevel);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ MockDevice/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MockDevice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TcpListener server = null;
+
+            try
+            {
+                server = new TcpListener(IPAddress.Parse("127.0.0.1"), 13175);
+                server.Start();
+                while (true)
+                {
+                    Console.WriteLine("Waiting for connection...");
+                    var client = server.AcceptTcpClient();
+                    Console.WriteLine("Client connected...");
+                    var stream = client.GetStream();
+
+
+                    var incomingBytes = new byte[256];
+                    var numberOfBytesRead = stream.Read(incomingBytes, 0, incomingBytes.Length);
+                    while (numberOfBytesRead != 0)
+                    {
+                        var incomingData = Encoding.ASCII.GetString(incomingBytes, 0, numberOfBytesRead);
+                        Console.WriteLine($"Data from client: {incomingData}");
+                        var outgoingBytes = Encoding.ASCII.GetBytes("~OUTPUT,2,1,48<CR><LF>");
+                        stream.Write(outgoingBytes, 0, outgoingBytes.Length);          
+                        numberOfBytesRead = stream.Read(incomingBytes, 0, incomingBytes.Length);
+                        Console.WriteLine("Bytes read...");
+                    }
+                    client.Close();
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+            finally
+            {
+                server?.Stop();
+            }
+        }
+    }
+}
